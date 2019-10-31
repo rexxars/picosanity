384c465974c2bb8c3adeee112ec37aef49ef74aa
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-10-31 17:30:03 -0400

    Fix browser fetch illegal invocation

--- src/browser.js
+++ src/browser.js
@@ -1,5 +1,5 @@
 const Client = require('./client')
 
 module.exports = function(cfg) {
-  return new Client(cfg, window.fetch)
+  return new Client(cfg, (input, init) => window.fetch(input, init))
 }
