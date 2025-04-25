50d1588241d1daae0bbbd0c81bee623449b50a0e
Espen Hovlandsdal <espen@hovlandsdal.com>
2025-04-25 08:54:31 -0700

    refactor: bump user agent version

--- src/node.js
+++ src/node.js
@@ -1,6 +1,6 @@
 import {createClient as create} from './client.js'
 
-const USER_AGENT = 'picosanity@4.1.1'
+const USER_AGENT = 'picosanity@5.0.0'
 
 export function createClient(cfg) {
   return create(cfg, {
